7d08a5943e65bb0247a3fa260ab563cf1b15febb
Athelon DaGeek <GeekAthelon@gmail.com>
2015-05-08 20:13:03 -0400

    Dead code removal

--- www/js/main.js
+++ www/js/main.js
@@ -47,16 +47,6 @@ requirejs(["wireup-qunit"], function (wireQUnit) {
             },
             // Update DOM on a Received Event
             receivedEvent: function (id) {
-                /*
-                var parentElement = document.getElementById(id);
-                var listeningElement = parentElement.querySelector('.listening');
-                var receivedElement = parentElement.querySelector('.received');
-
-                listeningElement.setAttribute('style', 'display:none;');
-                receivedElement.setAttribute('style', 'display:block;');
-
-                console.log('Received Event: ' + id);
-                */
 
             }
         };
