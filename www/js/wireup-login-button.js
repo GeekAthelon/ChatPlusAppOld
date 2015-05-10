5ff4cdef3ce89c233d8b56bc849ab529740b8600
Athelon DaGeek <GeekAthelon@gmail.com>
2015-05-10 00:07:08 -0400

    Basic handling of inline scripts.

--- www/js/wireup-login-button.js
+++ www/js/wireup-login-button.js
@@ -1,12 +1,12 @@
 /*global define:true */
 /*global Promise: true */
-
+/*global globalEval: true */
 
 define(["text!html/templates/login.html", "ajax"], function (template, ajax) {
     "use strict";
 
     function htmlToDom(html) {
-        return new Promise(function (resolve, reject) {
+        return new Promise(function (resolve /*, reject  */) {
 
             var doc = document.implementation.createHTMLDocument("example");
             doc.documentElement.innerHTML = html;
@@ -14,18 +14,26 @@ define(["text!html/templates/login.html", "ajax"], function (template, ajax) {
         });
     }
 
+    function injectScripts(scripts) {
+        var promises = scripts.map(function (script) {
 
-    function injectScripts(urls) {
-        var promises = urls.map(function (url) {
-            return ajax.get(url);
+            if (script.src) {
+                return ajax.get(script.src);
+            }
+
+            return new Promise(function (resolve /*, reject  */) {
+                resolve(script.code);
+            });
         });
 
         Promise.all(promises).then(function (codes) {
             codes.forEach(function (code, i) {
+                code = code.replace(/document.write/g, "DISALLOW DOCUMENT.WRITE");
+
                 try {
                     globalEval(code);
                 } catch (err) {
-                    window.alert("globalEval Error in script: " + urls[i] + "\r\n" + err);
+                    window.alert("globalEval Error in script: " + scripts[i] + "\r\n" + err);
                 }
             });
 
@@ -42,19 +50,15 @@ define(["text!html/templates/login.html", "ajax"], function (template, ajax) {
     function transferHtml(html) {
         var scripts = [];
         htmlToDom(html).then(function (doc) {
-
-            var txt = doc.body.innerHTML;
             document.body.innerHTML = "";
-
-            var origScriptCount = doc.getElementsByTagName("script").length;
 
             var node = doc.body.firstChild;
             while (node) {
-                var sib = node.nextSibling;
-
-                if (true && node.tagName && node.tagName.toLowerCase() === "script") {
+                if (node.tagName && node.tagName.toLowerCase() === "script") {
                     if (node.src) {
-                        scripts.push(node.src);
+                        scripts.push({src: node.src});
+                    } else {
+                        scripts.push({code: node.textContent});
                     }
                     node.parentNode.removeChild(node);
                 } else if (false && node.tagName && node.tagName.toLowerCase() === "link") {
@@ -65,14 +69,13 @@ define(["text!html/templates/login.html", "ajax"], function (template, ajax) {
                 node = doc.body.firstChild;
             }
 
-            //window.setTimeout(function () {
-                injectScripts(scripts);
-            //}, 500);
+            window.setTimeout(function() {
+            injectScripts(scripts);
+            }, 1000);
 
         }).catch(function (err) {
 
         });
-
     }
 
     function ajaxSubmit(e) {
@@ -104,8 +107,8 @@ define(["text!html/templates/login.html", "ajax"], function (template, ajax) {
         button.removeEventListener("click", handleClick, false);
         button.addEventListener("click", handleClick, false);
 
-        window.addEventListener("load", function() {
-            window.alert("init: load event called");
+        window.addEventListener("load", function () {
+            //window.alert("init: load event called");
         }, false);
     }
 
