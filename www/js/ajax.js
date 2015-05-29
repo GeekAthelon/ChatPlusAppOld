0b08257ac26f71f94231f3549bb986bbf9cb00a4
Athelon DaGeek <GeekAthelon@gmail.com>
2015-05-29 00:49:19 -0400

    Minor code cleanup to keep linter happy.

--- www/js/ajax.js
+++ www/js/ajax.js
@@ -10,9 +10,9 @@ define([], function () {
         var params = {}, tokens,
             re = /[?&]?([^=]+)=([^&]*)/g;
 
-        while (tokens = re.exec(qs)) {
-            params[decodeURIComponent(tokens[1])]
-                = decodeURIComponent(tokens[2]);
+        while ((tokens = re.exec(qs))) {
+            params[decodeURIComponent(tokens[1])] =
+                decodeURIComponent(tokens[2]);
         }
 
         return params;
